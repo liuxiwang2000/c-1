5d1d64811f0e76d38787fb2f9f27b438d469682f
刘希望 <1574721517@qq.com>
2014-09-07 14:45:29 +0800

    Initial Commit

---
+++ c++1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  c++1
+//
+//  Created by 刘希望 on 14-9-7.
+//  Copyright (c) 2014年 刘希望. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
